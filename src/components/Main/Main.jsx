edb1d961cb9911a8fed3e792830d7cc05e6e7427
jasnmar <jasnmar@gmail.com>
2025-03-31 09:56:29 -0400

    useState for the list of ingredients

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,38 @@
 import "./Main.css"
+import { useState } from "react"
+
+
 
 export default function Main() {
+  const [ingredients, setIngredients] = useState(["Chicken", "Oregano", "Tomatoes"])
+
+  const ingredientElements = ingredients.map(ingredient => {
+    return <li key={ingredient}>{ingredient}</li>
+  })
+  
+  function submitHandler(e) {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const newIngredient = formData.get("ingredient")
+    setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    console.log(ingredients)
+  }
+
+
   return (
     <>
     <main>
-      <form className="main--add-ingredient">
+      <form className="main--add-ingredient" onSubmit={submitHandler}>
         <input 
           type="text" 
           placeholder="e.g. Onion, Oregeno"
-          aria-label="Add ingredint" />
+          aria-label="Add ingredint"
+          name="ingredient" />
         <button>Add Ingredient</button>
       </form>
+      <ul>
+        {ingredientElements}
+      </ul>
     </main>
     </>
   )
